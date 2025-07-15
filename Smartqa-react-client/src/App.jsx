eb10bf6075c3b44bf8617a843fdf9a5b951536fa
Raunit2025 <raunitraj2336@gmail.com>
2025-07-15 15:06:02 +0530

    Testing commit through Shell

--- Smartqa-react-client/src/App.jsx
+++ Smartqa-react-client/src/App.jsx
@@ -1,15 +1,8 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-    <h1>Hello User...</h1>
-    <p>The App is under progress....</p>
-    <br />
-
-    <h3>Wait the developer is doing the server Part!</h3>
     </div>
   )
 }
