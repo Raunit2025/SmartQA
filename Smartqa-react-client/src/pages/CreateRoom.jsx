d17701e6ef21b4e88f08d1c43c735ccd375f8275
Raunit2025 <raunitraj2336@gmail.com>
2025-07-15 16:12:33 +0530

    Progress

--- Smartqa-react-client/src/pages/CreateRoom.jsx
+++ Smartqa-react-client/src/pages/CreateRoom.jsx
@@ -1,21 +1,41 @@
 import { useNavigate } from "react-router-dom";
+import { serverEndpoint } from "../config/appConfig";
+import axios from 'axios';
+import { useState } from "react";
 
 function CreateRoom() {
     const [name, setName] = useState(null);
     const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const validate = () => {
         let isValid = true;
         const newErrors = {};
 
-        if(name.length === 0) {
+        if (name.length === 0) {
             isValid = false;
             newErrors.name = "Name is Mandatory";
         }
 
         setErrors(newErrors);
         return isValid;
+    };
+
+    const handleSubmit = async () => {
+        if (validate()) {
+            try {
+                await axios.post(`${serverEndpoint}/room`, {
+                    createdBy: name
+                }, {
+                    withCredentials: true
+                });
+                navigate(`/room/${Response.data.roomCode}`);
+            } catch (error) {
+                console.log(error);
+                setErrors({ message: 'Error creating room, please try again' });
+            }
+        }
     };
 
     return (
